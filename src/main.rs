6eb663a0b4f805c8f36fa72e4d3c5d81202062ec
Manar Ali <ahmadshehadeh78@gmail.com>
2022-08-06 00:35:07 +0100

    Added new enum categories and added it to struct item

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ struct Item {
     name: String,
     description: String,
     rating: i8,
+    category: Categories,
     price: f32
 }
 
+enum Categories {
+    Toy,
+    Office,
+    Gaming,
+    Clothing,
+    Vehicle,
+    Gadget,
+    Other
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
